f55fec3ec318cb9a404ff28d157afb7a966e2bb1
Danial <danialdark12098@gmail.com>
2024-09-11 15:28:01 +0330

    sidebar finished

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -56,3 +56,79 @@ function minusProduct(element) {
         element.previousElementSibling.innerText = +count - 1
     }
 }
+
+
+// mobile
+
+var themeToggleDarkIconMobile = document.getElementById('theme-toggle-dark-icon-mobile');
+var themeToggleLightIconMobile = document.getElementById('theme-toggle-light-icon-mobile');
+var themeText = document.getElementById('theme-text');
+
+// Change the icons inside the button based on previous settings
+if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    themeToggleLightIconMobile.classList.remove('hidden');
+    themeText.innerText = "تم روشن"
+} else {
+    themeToggleDarkIconMobile.classList.remove('hidden');
+    themeText.innerText = "تم تاریک"
+
+}
+
+var themeToggleBtnMobile = document.getElementById('theme-toggle-mobile');
+
+themeToggleBtnMobile.addEventListener('click', function () {
+
+    // toggle icons inside button
+    themeToggleDarkIconMobile.classList.toggle('hidden');
+    themeToggleLightIconMobile.classList.toggle('hidden');
+
+    // if set via local storage previously
+    if (localStorage.getItem('color-theme')) {
+        if (localStorage.getItem('color-theme') === 'light') {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
+            themeText.innerText = "تم روشن"
+
+        } else {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
+            themeText.innerText = "تم تاریک"
+
+        }
+
+        // if NOT set via local storage previously
+    } else {
+        if (document.documentElement.classList.contains('dark')) {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
+            themeText.innerText = "تم روشن"
+        } else {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
+            themeText.innerText = "تم تاریک"
+        }
+    }
+
+});
+
+
+
+
+
+
+
+var sidebar = document.getElementById("sidebar")
+
+function closeSideBar() {
+    sidebar.classList.remove("right-0")
+    sidebar.classList.add("right-[-100%]")
+    document.getElementById("overlayBar").classList.add("hidden")
+}
+
+
+function openSideBar() {
+    sidebar.classList.add("right-0")
+    sidebar.classList.remove("right-[-100%]")
+    document.getElementById("overlayBar").classList.remove("hidden")
+}
+
